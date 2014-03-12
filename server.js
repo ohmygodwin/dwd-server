9c25f3a60ba999f68e8efe322e09d54a5127b024
Kate Godwin <mkategodwin@gatech.edu>
2014-03-12 19:16:19 -0400

    why did github correct bby to boy??????????? this is rude

--- server.js
+++
@@ -1,9 +0,0 @@
-var http = require('http');
-
-var server = http.createServer(function(req, res){
-	res.writeHead(200, {'Content-Type': 'text/html'});
-	res.end("<h1>hey y'all</h1>");
-});
-
-server.listen(3000, 'localhost');
-
