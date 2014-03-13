62dfa02da3fc7ba8895c7b3b27c31a5bc481a553
Kate Godwin <mkategodwin@gatech.edu>
2014-03-12 20:40:15 -0400

    changed dat port

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ var server = http.createServer(function(req, res){
 	res.writeHead(200, {'Content-Type': 'text/html'});
 	res.end("<h1>hey y'all</h1>");
 });
-
-server.listen(3000, 'localhost');
+var port = Number(process.env.PORT || 5000);
+server.listen(port);
 
