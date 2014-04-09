cf4d53820fe5d9e2a9f3530143ffd9e2bac74519
Kate Godwin <mkategodwin@gatech.edu>
2014-04-09 19:21:14 -0400

    adding handlebars

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
 var express = require('express');
+var expressHbs = require('express-handlebars');
+var handlebars = expressHbs.create();
+var app = express();
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+app.get('/', function(req, res){
+
+	res.render("hey, y'all");
+});
+
+app.use('/public', express.static('public'));
+
+app.listen(5000);
