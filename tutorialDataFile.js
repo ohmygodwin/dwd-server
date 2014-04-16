13d30bd653ff7a06d366c983aff1ba93e64c1716
Kate Godwin <mkategodwin@gatech.edu>
2014-04-16 18:54:02 -0400

    #each in handlebars

--- tutorialDataFile.js
+++ tutorialDataFile.js
@@ -1,11 +1,17 @@
 var data = {
 	'mushroom-dyes': {
 		name: 'Mushroom dyes',
-		materials: 'dye mushrooms'
+		materials: {
+			1: 'dye mushrooms',
+			2: 'appropriate mordant'
+		}
 	},
 	'spore-prints': {
 		name: 'Spore prints',
-		materials: 'gilled mushroom'
+		materials: {
+			1: 'gilled mushroom',
+			2: 'paper'
+		}
 	}
 };
 
